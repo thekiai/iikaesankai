b55924dc80667889fbf80ffa0bcf2af791df60b2
Ai Ozaki <aspara1024@gmail.com>
2024-03-16 08:20:02 +0900

    エラーメッセージ追加

--- frontend/src/components/ContentList.tsx
+++ frontend/src/components/ContentList.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
-import { VStack, Box, Center, Tabs, TabList, Tab, TabPanels, TabPanel, TabIndicator } from "@chakra-ui/react";
+import { VStack, Box, Center, Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import { ContentCard } from "./ContentCard";
 import { ContentType } from "../types/ContentType";
-import { API_ENDPOINT, mainColor, subColor } from "../assets/constants";
+import { API_ENDPOINT } from "../assets/constants";
 import { CustomSpinner } from "./CustomSpinner";
 
 const PER_PAGE = 5;
@@ -21,11 +21,13 @@ export const ContentList: React.FC = () => {
     const [rankingCurrentPage, setRankingCurrentPage] = useState(1);
     const [rankingHasNextPage, setRankingHasNextPage] = useState(true);
     const [rankingIsFetching, setRankingIsFetching] = useState(true);
+    const [rankingError, setRankingError] = useState(false);
 
     const [latestContents, setLatestContents] = useState<ContentType[]>([]);
     const [latestCurrentPage, setLatestCurrentPage] = useState(1);
     const [latestHasNextPage, setLatestHasNextPage] = useState(true);
     const [latestIsFetching, setLatestIsFetching] = useState(true);
+    const [latestError, setLatestError] = useState(false);
 
     const fetchRankingContents = async () => {
         try {
@@ -37,6 +39,7 @@ export const ContentList: React.FC = () => {
             setRankingCurrentPage((prevPage) => prevPage + 1);
         } catch (error) {
             console.error("Error fetching ranking data: ", error);
+            setRankingError(true);
             setRankingHasNextPage(false);
         } finally {
             setRankingIsFetching(false);
@@ -53,6 +56,7 @@ export const ContentList: React.FC = () => {
             setLatestCurrentPage((prevPage) => prevPage + 1);
         } catch (error) {
             console.error("Error fetching latest data: ", error);
+            setLatestError(true);
             setLatestHasNextPage(false);
         } finally {
             setLatestIsFetching(false);
@@ -85,70 +89,80 @@ export const ContentList: React.FC = () => {
 
                 <TabPanels>
                     <TabPanel>
-                        {rankingIsFetching && rankingCurrentPage === 1 ? (
-                            <Center> <CustomSpinner /> </Center>
-                        ) : (
-                            <InfiniteScroll
-                                dataLength={rankingContents.length}
-                                next={fetchMoreRankingData}
-                                hasMore={rankingHasNextPage}
-                                loader={<Center> <CustomSpinner /> </Center>}
-                                style={{ overflow: 'hidden' }}
-                            >
-                                <VStack maxWidth="500px">
-                                    {rankingContents.map((content: ContentType) => (
-                                        <Box
-                                            key={content.content_id}
-                                            borderBottom="2px"
-                                            pb={8}
-                                            borderColor="gray.200"
-                                            borderStyle="dashed"
-                                        >
-                                            <ContentCard
-                                                content_id={content.content_id}
-                                                who={content.who}
-                                                what={content.what}
-                                                detail={content.detail}
-                                                paraphrases={content.paraphrases}
-                                            />
-                                        </Box>
-                                    ))}
-                                </VStack>
-                            </InfiniteScroll>
-                        )}
+                        {rankingError ?
+                            <Center>
+                                <Box color="gray.600">
+                                    エラー発生😭リロードしてね
+                                </Box>
+                            </Center> :
+                            rankingIsFetching && rankingCurrentPage === 1 ?
+                                <Center mt={8}> <CustomSpinner /> </Center> :
+                                <InfiniteScroll
+                                    dataLength={rankingContents.length}
+                                    next={fetchMoreRankingData}
+                                    hasMore={rankingHasNextPage}
+                                    loader={<Center mt={8}> <CustomSpinner /> </Center>}
+                                    style={{ overflow: 'hidden' }}
+                                >
+                                    <VStack maxWidth="500px">
+                                        {rankingContents.map((content: ContentType) => (
+                                            <Box
+                                                key={content.content_id}
+                                                borderBottom="2px"
+                                                pb={8}
+                                                borderColor="gray.200"
+                                                borderStyle="dashed"
+                                            >
+                                                <ContentCard
+                                                    content_id={content.content_id}
+                                                    who={content.who}
+                                                    what={content.what}
+                                                    detail={content.detail}
+                                                    paraphrases={content.paraphrases}
+                                                />
+                                            </Box>
+                                        ))}
+                                    </VStack>
+                                </InfiniteScroll>
+                        }
                     </TabPanel>
                     <TabPanel>
-                        {latestIsFetching && latestCurrentPage === 1 ? (
-                            <Center> <CustomSpinner /> </Center>
-                        ) : (
-                            <InfiniteScroll
-                                dataLength={latestContents.length}
-                                next={fetchMoreLatestData}
-                                hasMore={latestHasNextPage}
-                                loader={<Center> <CustomSpinner /> </Center>}
-                                style={{ overflow: 'hidden' }}
-                            >
-                                <VStack maxWidth="500px">
-                                    {latestContents.map((content: ContentType) => (
-                                        <Box
-                                            key={content.content_id}
-                                            borderBottom="2px"
-                                            pb={8}
-                                            borderColor="gray.200"
-                                            borderStyle="dashed"
-                                        >
-                                            <ContentCard
-                                                content_id={content.content_id}
-                                                who={content.who}
-                                                what={content.what}
-                                                detail={content.detail}
-                                                paraphrases={content.paraphrases}
-                                            />
-                                        </Box>
-                                    ))}
-                                </VStack>
-                            </InfiniteScroll>
-                        )}
+                        {rankingError ?
+                            <Center>
+                                <Box color="gray.600">
+                                    エラー発生😭リロードしてね
+                                </Box>
+                            </Center> :
+                            latestIsFetching && latestCurrentPage === 1 ?
+                                <Center mt={8}> <CustomSpinner /> </Center> :
+                                <InfiniteScroll
+                                    dataLength={latestContents.length}
+                                    next={fetchMoreLatestData}
+                                    hasMore={latestHasNextPage}
+                                    loader={<Center mt={8}> <CustomSpinner /> </Center>}
+                                    style={{ overflow: 'hidden' }}
+                                >
+                                    <VStack maxWidth="500px">
+                                        {latestContents.map((content: ContentType) => (
+                                            <Box
+                                                key={content.content_id}
+                                                borderBottom="2px"
+                                                pb={8}
+                                                borderColor="gray.200"
+                                                borderStyle="dashed"
+                                            >
+                                                <ContentCard
+                                                    content_id={content.content_id}
+                                                    who={content.who}
+                                                    what={content.what}
+                                                    detail={content.detail}
+                                                    paraphrases={content.paraphrases}
+                                                />
+                                            </Box>
+                                        ))}
+                                    </VStack>
+                                </InfiniteScroll>
+                        }
                     </TabPanel>
                 </TabPanels>
             </Tabs >
